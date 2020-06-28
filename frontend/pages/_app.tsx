f66ef8c0442bfd79dbd7fa57ee66c40bae0beee7
andymuniz <munizandy94@gmail.com>
2020-06-28 18:51:38 -0500

    Cleanup files. Using Sanitize.css

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,4 +1,8 @@
-import '../styles.css';
+import 'sanitize.css';
+import 'sanitize.css/forms.css';
+import 'sanitize.css/typography.css';
+import 'sanitize.css/page.css';
+import '../global.css';
 
 export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
