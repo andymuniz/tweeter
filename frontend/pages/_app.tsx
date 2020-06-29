5e9f2458c2575e5bc76e111b1298b165fb1f75ed
andymuniz <munizandy94@gmail.com>
2020-06-29 00:48:51 -0500

    Fixing CSS imports. Adding other basic styles.

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,7 +2,7 @@ import 'sanitize.css';
 import 'sanitize.css/forms.css';
 import 'sanitize.css/typography.css';
 import 'sanitize.css/page.css';
-import '../global.css';
+import '../styles/global.css';
 
 export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
